1f0fe56ef33cf14dcc745ec6c5475fcd534c4319
Kenzie Bottoms <kenziebottoms@gmail.com>
2018-04-14 18:49:10 -0500

    added images and reaction text to ranges of scores; weighted words

--- assets/js/events.js
+++ assets/js/events.js
@@ -3,6 +3,11 @@
 const error = require("./error");
 const git = require("./git");
 const { rate } = require("./language");
+const G = `<br/><img src='assets/img/g.png'>`;
+const PG = `<br/><img src='assets/img/pg.png'>`;
+const PG13 = `<br/><img src='assets/img/pg13.png'>`;
+const R = `<br/><img src='assets/img/r.png'>`;
+const NC17 = `<br/><img src='assets/img/nc17.png'>`;
 
 // activate all event listeners
 module.exports.activateListeners = () => {
@@ -26,7 +31,20 @@ const search = event => {
         let [user, repo] = term.split("/");
         git.getWords(user, repo)
             .then(words => {
-                console.log(rate(words));
+                let rating = rate(words).toFixed(3);
+                if (rating > 0.15) {
+                    $("#output").html(`Holy shit. This repo scored ${rating}. You must have dropped a fuck bomb every other goddamn word.${NC17}`);
+                } else if (rating > 0.1) {
+                    $("#output").html(`Damn. This repo scored ${rating}. I hope you don't have to spend much time around children.${R}`);
+                } else if (rating > 0.01) {
+                    $("#output").html(`Okay. This repo scored ${rating}. A dev's gotta swear when a dev's gotta swear.${PG13}`);
+                } else if (rating > 0.001) {
+                    $("#output").html(`Nice. This repo scored ${rating}. I bet you wear button-ups to work.${PG}`);
+                } else if (rating > 0) {
+                    $("#output").html(`Wow. This repo scored ${rating}. What did you do, forget these were public?${G}`);
+                } else if (rating == 0) {
+                    $("#output").html(`Duuuude!! This repo scored ${rating}. You must be mature. Not jealous.${G}`);
+                }
             })
             .catch(err => {
                 console.log(err);
--- assets/js/language.js
+++ assets/js/language.js
@@ -2,13 +2,52 @@
 
 const _ = require("lodash");
 
-const badWords = [/fuck/g, /shit/g, /dick/g, /hell/g, /crap/g, /dang/g, /damn/g, /doggone/g, /[^a-zA-Z]+bs[^a-zA-Z]+/g, /heck/g, /shoot/g];
+const badWords = [
+  {
+    pattern: /fuck/gi,
+    severity: 1
+  }, {
+    pattern: /shit/gi,
+    severity: 0.9
+  }, {
+    pattern: /cock/gi,
+    severity: 0.9
+  }, {
+    pattern: /dick/gi,
+    severity: 0.7
+  }, {
+    pattern: /hell/gi,
+    severity: 0.3
+  }, {
+    pattern: /crap/gi,
+    severity: 0.1
+  }, {
+    pattern: /dang/gi,
+    severity: 0.05
+  }, { 
+    pattern: /damn/gi,
+    severity: 0.1
+  }, {
+    pattern: /doggone/gi,
+    severity: 0.05
+  }, {
+    pattern: /[^a-zA-Z]+bs[^a-zA-Z]+/gi,
+    severity: 0.075
+  }, {
+    pattern: /heck/gi,
+    severity: 0.5
+  }, {
+    pattern: /shoot/gi,
+    severity: 0.25
+  }
+];
 
 module.exports.rate = words => {
   let paragraph = words.join(" ");
-  let occs = [];
+  let score = 0;
   badWords.forEach(w => {
-    occs.push(paragraph.match(w));
+    let match = paragraph.match(w.pattern);
+    if (match) score += paragraph.match(w.pattern).length*w.severity;
   });
-  return (_.compact(_.flatten(occs)).length)/words.length;
+  return score/words.length;
 };
